5c4f07ef4b9e70733a701127b4aefbb576678c38
Bowe Strickland <bowe@redhat.com>
2019-04-27 01:07:00 -0400

    remove README.txt autoload

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
-    README = f.read()
 with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
@@ -28,7 +26,7 @@ tests_require = [
 setup(name='crowdpong-api',
       version='0.0',
       description='crowdpong-api',
-      long_description=README + '\n\n' + CHANGES,
+      long_description='Crowdpong API\n\n' + CHANGES,
       classifiers=[
           "Programming Language :: Python",
           "Framework :: Pyramid",
